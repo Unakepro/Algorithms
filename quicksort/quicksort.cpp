065122970351152e85832a9cb8739adb2adbdf63
Andrew <chebanov.andrew@gmail.com>
2023-11-03 20:29:10 +0100

    fixed quicksort

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -2,44 +2,44 @@
 #include <vector>
 
 
-void partition(int* arr, int size) {
-	int l = 1;
-	int r = size-1;
-	int start_elem = arr[0];	
+void quicksort(int* arr, int size) {
+
+	if(size > 1) {
+
+		int l = 1;
+		int r = size-1;
 	
-	while(l < r) {
-		if(arr[l] > start_elem) {
-			if(arr[r] < start_elem) {
-				std::swap(arr[l], arr[r]);
+		std::swap(arr[0], arr[size/2]);
+		int start_elem = arr[0];	
+		
+		while(l <= r) {
+			if(arr[l] > start_elem) {
+				if(arr[r] < start_elem) {
+					std::swap(arr[l], arr[r]);
+					++l;
+				}
+				else {
+						--r;
+				}
 			}
 			else {
-				--r;
+				++l;
 			}
 		}
-		else {
-			++l;
-		}
-	}
-	std::swap(arr[r], arr[0]);
-}
+		std::swap(arr[r], arr[0]);
 
-
-void quicksort(int* arr, int size) {
-	int half_size = size/2; 
-	if(size > 2) {
-		partition(arr, size);
-		quicksort(arr, half_size);
-		quicksort(arr+half_size, size-half_size);
+		quicksort(arr, r);
+		quicksort(arr+r+1, size-r-1);
 	}
 }
 
 
 
 int main() {
-	int arr[] = {10, 125, 17, 3, 14, 126, 2, 9, 21, 22, 3};
+	int arr[] = {1, 2,  9, 5, 22, 3};
 	int size = sizeof(arr)/sizeof(arr[0]);
 
-	partition(arr, size);
+	quicksort(arr, size);
 
 	for(int i = 0; i < size; ++i) {
 		std::cout << arr[i] << ' ';
