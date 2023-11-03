6b1c1c192491f1a9bf21e899836542df2e38ea93
Andrew <chebanov.andrew@gmail.com>
2023-11-03 20:58:19 +0100

    changed from cpp file to header

--- insertion_sort/insertion_sort.hpp
+++ insertion_sort/insertion_sort.hpp
@@ -1,12 +1,10 @@
+#ifndef INSERTION_SORT
+#define INSERTION_SORT
+
 #include <iostream>
 #include <algorithm>
 
-int main() {
-
-	int arr[] = {5, 7, 2, 1, 8, 12, 7};
-	
-	int size = sizeof(arr)/sizeof(arr[0]);
-	
+void insertion_sort(int* arr, int size) {
 	for(int i = 0; i < size; ++i) {
 		for(int j = i-1; j >= 0; --j) {
 			if(arr[j] > arr[j+1]) {
@@ -17,6 +15,9 @@ int main() {
 			}
 		}	
 	}
-
-	return 0;
 }
+
+
+
+
+#endif
