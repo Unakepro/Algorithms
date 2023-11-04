f60cb6ef7897919a70b1a22862fb67892bb3b64f
Andrew <chebanov.andrew@gmail.com>
2023-11-04 09:54:27 +0100

    heapsort in place

--- heapsort/heapsort.cpp
+++ heapsort/heapsort.cpp
@@ -2,61 +2,59 @@
 #include <vector>
 
 
-void fix(std::vector<int>& arr, int index) {
+void fix(int* arr, int size, int index) {
 	int l_child = 2*index+1;
 	int r_child = 2*index+2;
 
-	if(l_child < arr.size() && r_child < arr.size()) {
+	if(l_child < size && r_child < size) {
 		int max_child = arr[l_child] >= arr[r_child] ? l_child: r_child;
 
 		if(arr[index] < arr[max_child]) {
 			std::swap(arr[index], arr[max_child]);
-			fix(arr, max_child);	
+			fix(arr, size, max_child);	
 		}
 	}
-	else if(l_child < arr.size()) {
+	else if(l_child < size) {
 		if(arr[index] < arr[l_child]) {
 			std::swap(arr[index], arr[l_child]);
-			fix(arr, l_child);
+			fix(arr, size, l_child);
 		}
 	}
 }
 
 
 
-void make(std::vector<int>& arr) {
-	for(int i = arr.size()-1 ; i >= 0; --i) {
-		fix(arr, i);
+void make(int* arr, int size) {
+	for(int i = size-1 ; i >= 0; --i) {
+		fix(arr, size, i);
 	}
 }
 
 
 
-void heapsort(std::vector<int>& arr, std::vector<int>& output) {
+void heapsort(int* arr, int size) {
 
-	make(arr);
+	make(arr, size);
 
 
-	while(arr.size() != 0) {
-		
-		output[arr.size()-1] = arr[0];
-		
-		std::swap(arr[0], arr[arr.size()-1]);
-		arr.pop_back();
-		fix(arr, 0);
+	while(size != 0) {
+			
+		std::swap(arr[0], arr[size-1]);
+		--size;
+		fix(arr, size,  0);
 	}
 }
 
 
 
 int main() {
-	std::vector<int> xs = {2, 15, 37, 141, 26, 13, 174};
-	std::vector<int> output(xs.size());
+	int arr[] = {2, 15, 37, 141, 26, 13, 174};
+	int size = sizeof(arr)/sizeof(arr[0]);
 	
-	heapsort(xs, output);
+	heapsort(arr, size);
 
-	for(int i = 0; i < output.size(); ++i) {
-		std::cout << output[i] << ' ';
+	for(int i = 0; i < size; ++i) {
+		std::cout << arr[i] << ' ';
 	}
 		
 
