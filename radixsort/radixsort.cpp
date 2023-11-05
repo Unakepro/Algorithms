9abbe54d915f97bea60697067eda5c81d1a8c9c6
Andrew <chebanov.andrew@gmail.com>
2023-11-05 15:05:30 +0100

    change radisort a little bit

--- radixsort/radixsort.cpp
+++ radixsort/radixsort.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 #include <cmath>
 #include <climits>
 
 constexpr int decimal = 10;
 
-void counting_sort(int* arr, int size, int* output, int* pos, int pos_size, int k) {
+void counting_sort(int* arr, int size, int* output, int k) {
 
-	memset(pos, 0, pos_size*sizeof(pos[0]));		
+    int pos[decimal] = {};
 
 	for(int incr_elem = 0; incr_elem < size; ++incr_elem) {
 		pos[arr[incr_elem]/k%10] += 1;
 	}
 	
-	for(int add_elem = 0; add_elem < pos_size; ++add_elem) {
+	for(int add_elem = 1; add_elem < decimal; ++add_elem) {
 		pos[add_elem] += pos[add_elem-1];
 	}
 
-	for(int i = size-1; i >= 0; --i) {		
+	for(int i = size-1; i >= 0; --i) {
+		pos[arr[i]/k%10] -= 1;
+		output[pos[arr[i]/k%10]] = arr[i];
+	}
 
-		--pos[arr[i]/k%10];
-		output[pos[arr[i]/k%10]] = arr[i]; 
+    for(int count = 0; count < size; ++count) {
+        arr[count] = output[count];
+    }
 
-	}	
 }
 
 
@@ -31,10 +33,9 @@ void counting_sort(int* arr, int size, int* output, int* pos, int pos_size, int 
 void radixsort(int* arr, int size) {
 	
 	int* output = new int[size];	
-	int* pos = new int[decimal];
 
 	
-	int least_elem = *(std::min_element(arr, arr+size));	
+	int least_elem = *(std::min_element(arr, arr+size));
 	int max_elem = *(std::max_element(arr, arr+size));
 
 
@@ -44,17 +45,13 @@ void radixsort(int* arr, int size) {
 
 	for(int i = 0; i < size; ++i) {
 		arr[i] += abs(least_elem)+1;
-	} 
+	}
 
 	max_elem += abs(least_elem)+1;
 
 	int div = 1;
 	while(max_elem > 0) {	
-		counting_sort(arr, size, output, pos, decimal, div);
-	
-		
-		std::copy(output, output+size, arr);
-		
+		counting_sort(arr, size, output, div);
 
 		div *= 10;
 		max_elem /= 10;	
@@ -62,16 +59,15 @@ void radixsort(int* arr, int size) {
 	
 	for(int i = 0; i < size; ++i) {
 		arr[i] -= abs(least_elem)+1;
-	} 
+	}
 	
 	delete [] output;
-	delete [] pos;	
 }
 
 
 
 int main() {
-	int arr[] = {100, -100000, -50, -1, 10000, 20, 22, 13}; 	
+	int arr[] = {-50000, -20, -3, 100, 10000, 20, 22, 13};
 
 	int size = sizeof(arr)/sizeof(arr[0]);
 
--- radixsort/radixsort.hpp
+++ radixsort/radixsort.hpp
@@ -3,72 +3,68 @@
 
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 #include <cmath>
 #include <climits>
 
 constexpr int decimal = 10;
 
-void counting_sort(int* arr, int size, int* output, int* pos, int pos_size, int k) {
+void counting_sort(int* arr, int size, int* output, int k) {
 
-	memset(pos, 0, pos_size*sizeof(pos[0]));		
+    int pos[decimal] = {};
 
-	for(int incr_elem = 0; incr_elem < size; ++incr_elem) {
-		pos[arr[incr_elem]/k%10] += 1;
-	}
-	
-	for(int add_elem = 0; add_elem < pos_size; ++add_elem) {
-		pos[add_elem] += pos[add_elem-1];
-	}
+    for(int incr_elem = 0; incr_elem < size; ++incr_elem) {
+        pos[arr[incr_elem]/k%10] += 1;
+    }
 
-	for(int i = size-1; i >= 0; --i) {		
+    for(int add_elem = 1; add_elem < decimal; ++add_elem) {
+        pos[add_elem] += pos[add_elem-1];
+    }
 
-		--pos[arr[i]/k%10];
-		output[pos[arr[i]/k%10]] = arr[i]; 
+    for(int i = size-1; i >= 0; --i) {
+        pos[arr[i]/k%10] -= 1;
+        output[pos[arr[i]/k%10]] = arr[i];
+    }
 
-	}	
+    for(int count = 0; count < size; ++count) {
+        arr[count] = output[count];
+    }
+
 }
 
 
 
 void radixsort(int* arr, int size) {
-	
-	int* output = new int[size];	
-	int* pos = new int[decimal];
 
-	
-	//int least_elem = *(std::min_element(arr, arr+size));	
-	int max_elem = *(std::max_element(arr, arr+size));
+    int* output = new int[size];
 
 
-	//if(least_elem + max_elem + 1 >= INT_MAX) {
-	//	throw std::runtime_error("Does not work with this range of values");
-	//}
+    int least_elem = *(std::min_element(arr, arr+size));
+    int max_elem = *(std::max_element(arr, arr+size));
 
-	//for(int i = 0; i < size; ++i) {
-	//	arr[i] += abs(least_elem)+1;
-	//} 
 
-	//max_elem += abs(least_elem)+1;
+    if(least_elem + max_elem + 1 >= INT_MAX) {
+        throw std::runtime_error("Does not work with this range of values");
+    }
 
-	int div = 1;
-	while(max_elem > 0) {	
-		counting_sort(arr, size, output, pos, decimal, div);
-	
-		
-		std::copy(output, output+size, arr);
-		
+    for(int i = 0; i < size; ++i) {
+        arr[i] += abs(least_elem)+1;
+    }
 
-		div *= 10;
-		max_elem /= 10;	
-	}
-	
-	//for(int i = 0; i < size; ++i) {
-	//	arr[i] -= abs(least_elem)+1;
-	//} 
-	
-	delete [] output;
-	delete [] pos;	
+    max_elem += abs(least_elem)+1;
+
+    int div = 1;
+    while(max_elem > 0) {
+        counting_sort(arr, size, output, div);
+
+        div *= 10;
+        max_elem /= 10;
+    }
+
+    for(int i = 0; i < size; ++i) {
+        arr[i] -= abs(least_elem)+1;
+    }
+
+    delete [] output;
 }
 
 
