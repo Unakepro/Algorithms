f9ce7b3b880f48ece465460f381b44fc7aa64770
Andrew <chebanov.andrew@gmail.com>
2023-11-10 17:36:47 +0100

    updated hybridsort

--- hybridsort/hybridsort.hpp
+++ hybridsort/hybridsort.hpp
@@ -4,43 +4,45 @@
 
 #include <iostream> 
 #include "../quicksort/quicksort.hpp"
-#include "../radixsort/radixsort.hpp"
-#include "../radixsort/radixsort_positive.hpp"
-
+#include "../insertion_sort/insertion_sort.hpp"
 
 void hybridsort(int* arr, int size) {
-    int min = arr[0];
-    int max = arr[0];
-    bool is_sorted = true;
+
+	if(size < 170) {
+		insertion_sort(arr, size);
+	}
+	else {
+	
+		if(size > 1) {
+
+			int l = 1;
+			int r = size-1;
 
 
-    for(int i = 0; i < size; ++i) {
-        if(arr[i] < min) {
-            min = arr[i];
-        }
-        if(arr[i] > max) {
-            max = arr[i];
-        }
-        if(i < size-1) {
-            if(arr[i] > arr[i + 1]) {
-                is_sorted = false;
-            }
-        }
-    }
-
-    if(!is_sorted) {
-        if((max - min) > 100) {
-            quicksort(arr, size);
-        }
-        else {
-            if(min >= 0) {
-                radixsort_p(arr, size);
-            }
-            else {  
-                radixsort(arr, size);
-            }
-        }
-    }
-}
+			std::uniform_int_distribution<int> dist_q(0, size-1);	
+			std::swap(arr[0], arr[dist_q(gen_q)]);
+			int start_elem = arr[0];	
+			
+			while(l <= r) {
+				if(arr[l] > start_elem) {
+					if(arr[r] < start_elem) {
+						std::swap(arr[l], arr[r]);
+						++l;
+					}
+					else {
+							--r;
+					}
+				}
+				else {
+					++l;
+				}
+			}
+			std::swap(arr[r], arr[0]);
+		
+			hybridsort(arr, r);
+			hybridsort(arr+r+1, size-r-1);
+		}
+	}
+}   
 
 #endif
